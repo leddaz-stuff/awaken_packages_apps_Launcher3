ac94a5c652d1eeec9b76a5bf3ef4b3094fbb14c6
jhonboy121 <alfredmathew05@gmail.com>
2022-03-15 18:55:35 +0530

    Launcher3: Show clear all button in recents overview
    idoybh (YAAP): Keep rightmost
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Change-Id: I02ccb39cfbaa987ce2b6a0ae81702e9a34fb215e
    Signed-off-by: Ratoriku <a1063021545@gmail.com>

--- quickstep/src/com/android/quickstep/TaskOverlayFactory.java
+++ quickstep/src/com/android/quickstep/TaskOverlayFactory.java
@@ -249,6 +249,11 @@ public class TaskOverlayFactory implements ResourceBasedOverride {
             overviewPanel.initiateSplitSelect(mThumbnailView.getTaskView());
         }
 
+        private void clearAllTasks() {
+            final RecentsView recentsView = mThumbnailView.getTaskView().getRecentsView();
+            recentsView.dismissAllTasks();
+        }
+
         /**
          * Called when the overlay is no longer used.
          */
@@ -343,20 +348,28 @@ public class TaskOverlayFactory implements ResourceBasedOverride {
             }
 
             @SuppressLint("NewApi")
+            @Override
             public void onScreenshot() {
                 endLiveTileMode(() -> saveScreenshot(mTask));
             }
 
+            @Override
             public void onSplit() {
                 endLiveTileMode(TaskOverlay.this::enterSplitSelect);
             }
 
+            @Override
             public void onLens() {
                 if (mIsAllowedByPolicy) {
                     endLiveTileMode(() -> mImageApi.startLensActivity());
                 } else {
                     showBlockedByPolicyMessage();
                 }
+            }
+
+            @Override
+            public void onClearAllTasksRequested() {
+                endLiveTileMode(TaskOverlay.this::clearAllTasks);
             }
         }
     }
@@ -373,5 +386,7 @@ public class TaskOverlayFactory implements ResourceBasedOverride {
         void onSplit();
 
         void onLens();
+
+        void onClearAllTasksRequested();
     }
 }
